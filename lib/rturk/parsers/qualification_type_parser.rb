01dbeff32ed06e1afd7b1ee3bc64daf650d74924
jmh <jim1138@gmail.com>
2011-08-28 06:23:58 -0400

    fixing qualification type fields for search results

--- lib/rturk/parsers/qualification_type_parser.rb
+++ lib/rturk/parsers/qualification_type_parser.rb
@@ -1,44 +1,18 @@
-#   <QualificationType>
-#     <QualificationTypeId>WKAZMYZDCYCZP412TZEZ</QualificationTypeId>
-#     <Name> WebReviews Qualification Master Test</Name>
-#     <Description>This qualification will allow you to earn more on the WebReviews HITs.</Description>
-#     <Keywords>WebReviews, webreviews, web reviews</Keywords>
-#     <QualificationTypeStatus>Active</QualificationTypeStatus>
-#     <CreationTime>2009-05-17T10:05:15Z</CreationTime>
-#     <Test>
-#       <QuestionForm xmlns="http://mechanicalturk.amazonaws.com/AWSMechanicalTurkDataSchemas/2005-10-01/QuestionForm.xsd">
-#         <Overview>
-#           <Title>WebReviews Survey</Title>
-#           <Text>After you have filled out this survey you will be assigned one or more qualifications...</Text>
-#         </Overview>
-#         <Question>
-#           <QuestionIdentifier>age</QuestionIdentifier>
-#           <DisplayName>What is your age?</DisplayName>
-#           <IsRequired>true</IsRequired>
-#           <QuestionContent><Text>Please choose the age group you belong to.</Text></QuestionContent>
-#           <AnswerSpecification>
-#             <SelectionAnswer>
-#               <StyleSuggestion>radiobutton</StyleSuggestion>
-#               <Selections>
-#                 <Selection><SelectionIdentifier>0018</SelectionIdentifier><Text>-18</Text></Selection>
-#                 <Selection><SelectionIdentifier>5160</SelectionIdentifier><Text>51-60</Text></Selection>
-#                 <Selection><SelectionIdentifier>6000</SelectionIdentifier><Text>60+</Text></Selection>
-#               </Selections>  
-#             </SelectionAnswer>
-#           </AnswerSpecification>
-#         </Question> 
-#       </QuestionForm>
-#     </Test>
-#     <TestDurationInSeconds>1200</TestDurationInSeconds>
-#   </QualificationType>
-
 module RTurk
-
+  # <QualificationType>
+  #   <QualificationTypeId>2TP5XH0JPPSINVFMQYJPA5KLM7ZMGT</QualificationTypeId>
+  #   <CreationTime>2011-06-16T20:09:56Z</CreationTime>
+  #   <Name>Product Search Relevance Qualification Test </Name>
+  #   <Description>CLOSED HIT</Description>
+  #   <Keywords>search, relevance, product search, queries, search relevance, china, chinese, CN</Keywords>
+  #   <QualificationTypeStatus>Active</QualificationTypeStatus>
+  #   <RetryDelayInSeconds>86000</RetryDelayInSeconds>
+  # </QualificationType>
+  
+  # for returns from SearchQualificationTypes operation
   class QualificationTypeParser < RTurk::Parser
     attr_reader :qualification_type_id, :creation_time, :name, :description,
-                :keywords, :status, :retry_delay_in_seconds, :is_requestable,
-                :test, :test_duration_in_seconds, :answer_key, :auto_granted,
-                :auto_granted_value
+                :keywords, :status, :retry_delay_in_seconds
 
     def initialize(hit_xml)
       @xml_obj = hit_xml
@@ -49,14 +23,7 @@ module RTurk
         :description => 'Description',
         :keywords => 'Keywords',
         :status => 'QualificationTypeStatus',
-        :retry_delay_in_seconds => 'RetryDelayInSeconds',
-        :is_requestable => 'IsRequestable',
-        :test => 'Test',
-        :test_duration_in_seconds => 'TestDurationInSeconds',
-        :answer_key => 'AnswerKey',
-        :auto_granted => 'AutoGranted',
-        :auto_granted_value => 'AutoGrantedValue'
-      )
+        :retry_delay_in_seconds => 'RetryDelayInSeconds')
 
       @keywords = @keywords.split(', ') if @keywords
     end
